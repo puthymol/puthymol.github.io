df1f0e271436028108516bbe14a3070fb08f53b9
Puthy <puthymol@gmail.com>
2021-02-22 17:23:18 +0700

    Update

--- script/script.js
+++ script/script.js
@@ -1,26 +1,5 @@
-function openNav() {
-
-    let width = window.screen.width;
-    console.log(width)
-    if(width <560){
-        document.getElementById("sidenav").style.width = width+"px";
-        document.getElementById("main").style.marginLeft = width+"px";
-    }else{
-        document.getElementById("sidenav").style.width = "350px";
-        document.getElementById("main").style.marginLeft = "350px";
-    }
-
-    document.getElementById("btn-open").style.display = "none";
-}
-
-function closeNav() {
-    document.getElementById("sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("btn-open").style.display = "inline";
-    document.body.style.backgroundColor = "white";
-}
-
 $(document).ready(function(){
+
     $(document).scroll(function() {
         let y = $(this).scrollTop();
         if (y > 400) {
@@ -29,17 +8,38 @@ $(document).ready(function(){
             $('#scroll-top').fadeOut();
         }
 
-
         let scrollPos = y;
         $('#sidenav ul li a').each(function () {
             let currLink = $(this);
             let refElement = $(currLink.attr("href"));
-            if (refElement.position()?.top <= scrollPos && refElement.position()?.top + refElement.height() > scrollPos) {
+            console.log("scrollPOS", scrollPos);
+            console.log("Height", refElement.height());
+            console.log("Top", refElement.position()?.top);
+            if (refElement.position()?.top <= scrollPos && refElement.position()?.top + refElement.height() > scrollPos ) {
+                $('#sidenav ul li a').removeClass("active");
                 currLink.addClass("active");
-            }
-            else{
+            }else{
                 currLink.removeClass("active");
             }
+        });
+        
+    });
+    $( "#sidenav ul li a" ).click(function() {
+        let width = window.screen.width;
+        if(width <560) closeNav();
+        $('#sidenav ul li a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        let target = this.hash, menu = target;
+        $target = $(target);
+        let scrollTop = $target.offset().top;
+        if(width<560) scrollTop -= 70
+        $('html, body').stop().animate({
+            'scrollTop': scrollTop
+        }, 'smooth', function () {
+            window.location.hash = target;
         });
     });
     $( "#scroll-top" ).click(function() {
@@ -49,13 +49,23 @@ $(document).ready(function(){
             behavior: 'smooth'
         });
     });
-
-
-    $(document).on("scroll", onScroll);
 });
 
+function openNav() {
+    let width = window.screen.width;
+    if(width <560){
+        document.getElementById("sidenav").style.width = "100%";
+    }else{
+        document.getElementById("sidenav").style.width = "350px";
+        document.getElementById("main").style.marginLeft = "350px";
+    }
+    document.getElementById("main").style.paddingTop= "0";
+    document.getElementById("header").style.display = "none";
+}
 
-function onScroll(event){
-    console.log("Yo")
-
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.paddingTop= "80px";
+    document.getElementById("header").style.display = "inline";
 }
